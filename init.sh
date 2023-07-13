53ec01206bb38bfb74e7766bfd2eb8cbf6347916
Kevin Minehart <kmineh0151@gmail.com>
2023-07-13 13:09:41 -0500

    add .zshrc

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ mkdir -p $HOME/.config/nvim
 ln -s $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -s $PWD/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
 ln -s $PWD/sway $HOME/.config/sway
+ln -s $PWD/.zshrc $HOME/.zshrc
