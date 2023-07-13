574dc2d3f8fe08419ae0e4a57e062bd134e42d6e
Kevin Minehart <kmineh0151@gmail.com>
2023-07-13 13:30:53 -0500

    add alacritty

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ mkdir -p $HOME/.config/nvim
 ln -s $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -s $PWD/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
 ln -s $PWD/sway $HOME/.config/sway
+ln -s $PWD/alacritty $HOME/.config/alacritty
 ln -s $PWD/.zshrc $HOME/.zshrc
